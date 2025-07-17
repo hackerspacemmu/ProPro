8e4800aead58bdba20985420986b3323686ecbe5
SOO <ssj060707+github@gmail.com>
2025-07-17 16:10:13 +0800

    Multiple patches: mispelling in routes renamed proposals table mmu_directory no longer mandatory

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get "user/sign_in"
   resource :session
   resources :passwords, param: :token
-  resouces :courses, only: [:show] 
+  resources :courses, only: [:show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
