ee2bd1268d830bea122237482e27361e4b5391e1
Alex Chiam <chiamjuinhoong@gmail.com>
2025-07-17 13:20:59 +0800

    feat: fixed naming proposal templates to project models

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,28 +1,8 @@
 class CoursesController < ApplicationController
-    before_action :set_course_name
-    before_action :set_group_by_course
-    before_action :set_proposal 
 
-    def viewcourse
-        @course = {
-            name: @course,
-            group: @group,
-            proposal: @proposal,
-            status: @proposal.status,
-            members: @group.users
-        }
-    end
-
-    private 
-    def set_course_name
+    def show
         @course = Course.find(params[:id])
-    end 
-
-    def set_group_by_course
         @group = current_user.project_groups.joins(:enrolments).find_by(enrolments: { course_id: @course.id })
-    end
-
-    def set_proposal 
         @proposal = Proposal.find_by(group_id: @group.id)
-    end
-end 
+    end
+end
