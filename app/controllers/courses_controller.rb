4179051d74419ff25f1b37d4ffe3e7a5c3dcda4d
Alex Chiam <chiamjuinhoong@gmail.com>
2025-07-24 00:12:45 +0800

    feat: controller for show/courses, js for progress indicator, style for show courses (not routed yet)

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -2,9 +2,30 @@ class CoursesController < ApplicationController
 
     def show
         @course = Course.find(params[:id])
-        @group = current_user.project_groups.joins(:enrolments).find_by(enrolments: { course_id: @course.id })
-        @project = Project.find_by(group_id: @group.id)
-        @project_template = @course.project_template
-        @lecturer = supervisor.projects.where(enrolment: { course_id: @course.id }).count
+        
+        if @course.grouped?
+            @group = current_user.project_groups.joins(:project_group_members).find_by(project_group_members: {course_id: @course.id})
+            @project = Project.find_by(owner: @group)
+            @group_list = @course.project_group
+
+        else:
+            @group = nil
+            @project = Project.find_by(owner: current_user, course_id: @course.id)
+
+        @description = @course.project_template.description
+        @student_list = @course.enrolments.where(role: :student).includes(:user).map(&:user)
+        @lecturers = @course.enrolments.where(role: :lecturer).includes(:user).map(&:user)
+        @topic_list = @course.projects.joins(:ownership).where(ownerships: { ownership_type: :lecturer })
+        @students_with_projects = @student_list.select do |student|students_with_projects.include?(student.id)
+        @students_without_projects = @student_list.reject do |student|students_with_projects.include?(student.id)
+
     end
+    private 
+    def students_with_projects
+        Project.joins(:ownership).where(course_id: @course.id, ownerships: { ownership_type: :student, owner_type: "User" })
+        .where.not(status: :rejected)
+        .pluck("ownerships.owner_id")
+    end
+
+
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -4,7 +4,7 @@ class Project < ApplicationRecord
   belongs_to :owner, polymorphic: true
   delegate :course, to: :enrolment
 
-  enum :status, { pending: 0, approved: 1, rejected: 2 }
+  enum :status, { pending: 0, approved: 1, rejected: 2, redo: 3 }
 
   has_one :course, through: :enrolment
 
