7bae296dd009ff3101fa1e795a074898e5f2dcf3
SOO <ssj060707+github@gmail.com>
2025-08-05 21:49:20 +0800

    fixed comments and course access

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -11,18 +11,22 @@ class CommentsController < ApplicationController
       return
     end
 
-    if !parent_course.grouped
-      unless Current.user == User.find(parent_project.ownership.owner_id) || Current.user == parent_project.supervisor || Current.user == parent_course.coordinator.user
-        redirect_to "/"
-        return
-      end
-    else
-      group_members = ProjectGroup.find(parent_project.ownership.owner_id).project_group_members.map { |member| User.find(member.user_id) }
+    whitelist = [parent_course.coordinator.user, parent_project.supervisor]
 
-      unless group_members.includes? Current.user || Current.user == parent_project.supervisor || Current.user == parent_course.coordinator.user
-        redirect_to "/"
-        return
+    if type == :student
+      if !parent_course.grouped
+        whitelist.push(parent_project.owner)
+      else
+        group_members = ProjectGroup.find(parent_project.ownership.owner_id).project_group_members
+
+        group_members.each do |group_member|
+          whitelist.push(group_member.user)
+        end
       end
+    end
+
+    unless whitelist.include? Current.user
+      return
     end
 
     Comment.create!(
@@ -60,5 +64,3 @@ class CommentsController < ApplicationController
 
   end
 end
-
-
