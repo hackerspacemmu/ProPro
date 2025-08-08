7fa8ddeeaac477e23d1ab5c4f720c0ffe6cc2af5
Alex Chiam <chiamjuinhoong@gmail.com>
2025-08-08 16:58:44 +0800

    feat: skinnifying courses/show and moved assigned topics to projects model with :scope

--- app/models/project.rb
+++ app/models/project.rb
@@ -11,6 +11,11 @@ class Project < ApplicationRecord
   delegate :owner, to: :ownership
   enum :status, { pending: 0, approved: 1, rejected: 2, redo: 3 }
 
+  scope :pending_for_lecturer, ->(lecturer_enrolment) {
+  includes(:ownership, :supervisor_enrolment).where(status: 'pending', supervisor_enrolment: lecturer_enrolment).joins(:ownership)
+    .where.not(ownerships: { ownership_type: Ownership.ownership_types[:lecturer] })
+  }
+
   def supervisor
     User.find(Enrolment.find(self.enrolment_id).user_id)
   end
@@ -22,4 +27,5 @@ class Project < ApplicationRecord
       [ ownership.user ]
     end
   end
+  
 end
