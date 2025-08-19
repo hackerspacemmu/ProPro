95def5f1faecdb000e588d6320ac8cf7ccd34cae
Alex Chiam <chiamjuinhoong@gmail.com>
2025-08-20 00:44:15 +0800

    feat: added tabbing for project details and progress updates

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -18,4 +18,12 @@ module ProjectsHelper
 
     false
   end
+
+  def current_tab
+    params[:tab] || 'details'
+  end
+
+  def show_progress_tab?
+    @course.use_progress_updates && @current_instance.status == "approved"
+  end
 end
