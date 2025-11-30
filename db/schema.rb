d3b8ec20fa6da0571448c805cd14c0dfbc4fefef
SOO <ssj060707+github@gmail.com>
2025-11-30 22:44:38 +0800

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,91 +10,94 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_02_125437) do
+ActiveRecord::Schema[8.1].define(version: 2025_10_02_125437) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
+
   create_table "comments", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.string "text", null: false
     t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.boolean "deleted", default: false, null: false
-    t.string "location_type", null: false
     t.integer "location_id", null: false
+    t.string "location_type", null: false
+    t.string "text", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
     t.index ["location_type", "location_id"], name: "index_comments_on_location"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "courses", force: :cascade do |t|
-    t.string "course_name", null: false
-    t.integer "number_of_updates"
-    t.integer "starting_week", null: false
-    t.boolean "grouped", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "supervisor_projects_limit", null: false
-    t.boolean "require_coordinator_approval", null: false
-    t.integer "student_access", null: false
-    t.boolean "lecturer_access", null: false
-    t.boolean "use_progress_updates", null: false
     t.string "course_description"
-    t.string "file_link"
+    t.string "course_name", null: false
     t.string "coursecode"
+    t.datetime "created_at", null: false
+    t.string "file_link"
+    t.boolean "grouped", null: false
+    t.boolean "lecturer_access", null: false
+    t.integer "number_of_updates"
+    t.boolean "require_coordinator_approval", null: false
+    t.integer "starting_week", null: false
+    t.integer "student_access", null: false
+    t.integer "supervisor_projects_limit", null: false
+    t.datetime "updated_at", null: false
+    t.boolean "use_progress_updates", null: false
     t.index ["coursecode"], name: "index_courses_on_coursecode", unique: true
   end
 
   create_table "enrolments", force: :cascade do |t|
-    t.integer "user_id", null: false
     t.integer "course_id", null: false
-    t.integer "role", null: false
     t.datetime "created_at", null: false
+    t.integer "role", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
     t.index ["course_id"], name: "index_enrolments_on_course_id"
     t.index ["user_id"], name: "index_enrolments_on_user_id"
   end
 
   create_table "otps", force: :cascade do |t|
-    t.string "otp", null: false
     t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string "otp", null: false
     t.string "token", null: false
+    t.datetime "updated_at", null: false
     t.integer "user_id", null: false
     t.index ["user_id"], name: "index_otps_on_user_id"
   end
 
   create_table "progress_updates", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.date "date"
+    t.string "feedback", null: false
     t.integer "project_id", null: false
     t.integer "rating", null: false
-    t.string "feedback", null: false
-    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.date "date"
     t.index ["project_id"], name: "index_progress_updates_on_project_id"
   end
 
   create_table "project_group_members", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "project_group_id", null: false
     t.datetime "created_at", null: false
+    t.integer "project_group_id", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
     t.index ["project_group_id"], name: "index_project_group_members_on_project_group_id"
     t.index ["user_id"], name: "index_project_group_members_on_user_id"
   end
 
   create_table "project_groups", force: :cascade do |t|
     t.integer "course_id", null: false
-    t.string "group_name", null: false
     t.datetime "created_at", null: false
+    t.string "group_name", null: false
     t.datetime "updated_at", null: false
     t.index ["course_id"], name: "index_project_groups_on_course_id"
   end
 
   create_table "project_instance_fields", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.integer "instance_id"
+    t.string "instance_type"
     t.integer "project_instance_id"
     t.integer "project_template_field_id", null: false
-    t.text "value"
-    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "instance_type"
-    t.integer "instance_id"
+    t.text "value"
     t.index ["instance_type", "instance_id"], name: "index_project_instance_fields_on_instance"
     t.index ["project_instance_id", "project_template_field_id"], name: "index_project_instance_fields_on_instance_and_template_field", unique: true
     t.index ["project_instance_id"], name: "index_project_instance_fields_on_project_instance_id"
@@ -102,21 +105,21 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_02_125437) do
   end
 
   create_table "project_instances", force: :cascade do |t|
-    t.integer "project_id", null: false
-    t.integer "version", null: false
-    t.integer "created_by_id", null: false
-    t.datetime "submitted_at"
     t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "title", null: false
-    t.integer "status", default: 0, null: false
+    t.integer "created_by_id", null: false
     t.integer "enrolment_id"
-    t.integer "source_topic_id"
-    t.integer "project_instance_type", null: false
-    t.datetime "last_status_change_time"
+    t.integer "last_edit_by"
     t.datetime "last_edit_time"
     t.integer "last_status_change_by"
-    t.integer "last_edit_by"
+    t.datetime "last_status_change_time"
+    t.integer "project_id", null: false
+    t.integer "project_instance_type", null: false
+    t.integer "source_topic_id"
+    t.integer "status", default: 0, null: false
+    t.datetime "submitted_at"
+    t.string "title", null: false
+    t.datetime "updated_at", null: false
+    t.integer "version", null: false
     t.index ["created_by_id"], name: "index_project_instances_on_created_by_id"
     t.index ["enrolment_id"], name: "index_project_instances_on_enrolment_id"
     t.index ["project_id", "version"], name: "index_project_instances_on_project_id_and_version", unique: true
@@ -125,13 +128,13 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_02_125437) do
   end
 
   create_table "project_template_fields", force: :cascade do |t|
-    t.integer "project_template_id", null: false
-    t.integer "field_type", null: false
     t.integer "applicable_to", null: false
-    t.string "label", null: false
-    t.text "hint"
-    t.json "options"
     t.datetime "created_at", null: false
+    t.integer "field_type", null: false
+    t.text "hint"
+    t.string "label", null: false
+    t.json "options"
+    t.integer "project_template_id", null: false
     t.datetime "updated_at", null: false
     t.index ["project_template_id"], name: "index_project_template_fields_on_project_template_id"
   end
@@ -139,71 +142,71 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_02_125437) do
   create_table "project_templates", force: :cascade do |t|
     t.integer "course_id", null: false
     t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string "description"
+    t.datetime "updated_at", null: false
     t.index ["course_id"], name: "index_project_templates_on_course_id"
   end
 
   create_table "projects", force: :cascade do |t|
-    t.integer "enrolment_id"
     t.integer "course_id", null: false
     t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "status", default: 0, null: false
-    t.string "owner_type"
+    t.integer "enrolment_id"
     t.integer "owner_id"
+    t.string "owner_type"
     t.integer "ownership_type"
+    t.integer "status", default: 0, null: false
+    t.datetime "updated_at", null: false
     t.index ["course_id"], name: "index_projects_on_course_id"
     t.index ["enrolment_id"], name: "index_projects_on_enrolment_id"
     t.index ["owner_type", "owner_id"], name: "index_projects_on_owner"
   end
 
   create_table "sessions", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.string "ip_address"
-    t.string "user_agent"
     t.datetime "created_at", null: false
+    t.string "ip_address"
     t.datetime "updated_at", null: false
+    t.string "user_agent"
+    t.integer "user_id", null: false
     t.index ["user_id"], name: "index_sessions_on_user_id"
   end
 
   create_table "solid_queue_blocked_executions", force: :cascade do |t|
-    t.bigint "job_id", null: false
-    t.string "queue_name", null: false
-    t.integer "priority", default: 0, null: false
     t.string "concurrency_key", null: false
-    t.datetime "expires_at", null: false
     t.datetime "created_at", null: false
+    t.datetime "expires_at", null: false
+    t.bigint "job_id", null: false
+    t.integer "priority", default: 0, null: false
+    t.string "queue_name", null: false
     t.index ["concurrency_key", "priority", "job_id"], name: "index_solid_queue_blocked_executions_for_release"
     t.index ["expires_at", "concurrency_key"], name: "index_solid_queue_blocked_executions_for_maintenance"
     t.index ["job_id"], name: "index_solid_queue_blocked_executions_on_job_id", unique: true
   end
 
   create_table "solid_queue_claimed_executions", force: :cascade do |t|
+    t.datetime "created_at", null: false
     t.bigint "job_id", null: false
     t.bigint "process_id"
-    t.datetime "created_at", null: false
     t.index ["job_id"], name: "index_solid_queue_claimed_executions_on_job_id", unique: true
     t.index ["process_id", "job_id"], name: "index_solid_queue_claimed_executions_on_process_id_and_job_id"
   end
 
   create_table "solid_queue_failed_executions", force: :cascade do |t|
-    t.bigint "job_id", null: false
-    t.text "error"
     t.datetime "created_at", null: false
+    t.text "error"
+    t.bigint "job_id", null: false
     t.index ["job_id"], name: "index_solid_queue_failed_executions_on_job_id", unique: true
   end
 
   create_table "solid_queue_jobs", force: :cascade do |t|
-    t.string "queue_name", null: false
-    t.string "class_name", null: false
-    t.text "arguments"
-    t.integer "priority", default: 0, null: false
     t.string "active_job_id"
-    t.datetime "scheduled_at"
-    t.datetime "finished_at"
+    t.text "arguments"
+    t.string "class_name", null: false
     t.string "concurrency_key"
     t.datetime "created_at", null: false
+    t.datetime "finished_at"
+    t.integer "priority", default: 0, null: false
+    t.string "queue_name", null: false
+    t.datetime "scheduled_at"
     t.datetime "updated_at", null: false
     t.index ["active_job_id"], name: "index_solid_queue_jobs_on_active_job_id"
     t.index ["class_name"], name: "index_solid_queue_jobs_on_class_name"
@@ -213,92 +216,92 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_02_125437) do
   end
 
   create_table "solid_queue_pauses", force: :cascade do |t|
-    t.string "queue_name", null: false
     t.datetime "created_at", null: false
+    t.string "queue_name", null: false
     t.index ["queue_name"], name: "index_solid_queue_pauses_on_queue_name", unique: true
   end
 
   create_table "solid_queue_processes", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "hostname"
     t.string "kind", null: false
     t.datetime "last_heartbeat_at", null: false
-    t.bigint "supervisor_id"
-    t.integer "pid", null: false
-    t.string "hostname"
     t.text "metadata"
-    t.datetime "created_at", null: false
     t.string "name", null: false
+    t.integer "pid", null: false
+    t.bigint "supervisor_id"
     t.index ["last_heartbeat_at"], name: "index_solid_queue_processes_on_last_heartbeat_at"
     t.index ["name", "supervisor_id"], name: "index_solid_queue_processes_on_name_and_supervisor_id", unique: true
     t.index ["supervisor_id"], name: "index_solid_queue_processes_on_supervisor_id"
   end
 
   create_table "solid_queue_ready_executions", force: :cascade do |t|
-    t.bigint "job_id", null: false
-    t.string "queue_name", null: false
-    t.integer "priority", default: 0, null: false
     t.datetime "created_at", null: false
+    t.bigint "job_id", null: false
+    t.integer "priority", default: 0, null: false
+    t.string "queue_name", null: false
     t.index ["job_id"], name: "index_solid_queue_ready_executions_on_job_id", unique: true
     t.index ["priority", "job_id"], name: "index_solid_queue_poll_all"
     t.index ["queue_name", "priority", "job_id"], name: "index_solid_queue_poll_by_queue"
   end
 
   create_table "solid_queue_recurring_executions", force: :cascade do |t|
-    t.bigint "job_id", null: false
-    t.string "task_key", null: false
-    t.datetime "run_at", null: false
     t.datetime "created_at", null: false
+    t.bigint "job_id", null: false
+    t.datetime "run_at", null: false
+    t.string "task_key", null: false
     t.index ["job_id"], name: "index_solid_queue_recurring_executions_on_job_id", unique: true
     t.index ["task_key", "run_at"], name: "index_solid_queue_recurring_executions_on_task_key_and_run_at", unique: true
   end
 
   create_table "solid_queue_recurring_tasks", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "schedule", null: false
-    t.string "command", limit: 2048
-    t.string "class_name"
     t.text "arguments"
-    t.string "queue_name"
-    t.integer "priority", default: 0
-    t.boolean "static", default: true, null: false
-    t.text "description"
+    t.string "class_name"
+    t.string "command", limit: 2048
     t.datetime "created_at", null: false
+    t.text "description"
+    t.string "key", null: false
+    t.integer "priority", default: 0
+    t.string "queue_name"
+    t.string "schedule", null: false
+    t.boolean "static", default: true, null: false
     t.datetime "updated_at", null: false
     t.index ["key"], name: "index_solid_queue_recurring_tasks_on_key", unique: true
     t.index ["static"], name: "index_solid_queue_recurring_tasks_on_static"
   end
 
   create_table "solid_queue_scheduled_executions", force: :cascade do |t|
-    t.bigint "job_id", null: false
-    t.string "queue_name", null: false
-    t.integer "priority", default: 0, null: false
-    t.datetime "scheduled_at", null: false
     t.datetime "created_at", null: false
+    t.bigint "job_id", null: false
+    t.integer "priority", default: 0, null: false
+    t.string "queue_name", null: false
+    t.datetime "scheduled_at", null: false
     t.index ["job_id"], name: "index_solid_queue_scheduled_executions_on_job_id", unique: true
     t.index ["scheduled_at", "priority", "job_id"], name: "index_solid_queue_dispatch_all"
   end
 
   create_table "solid_queue_semaphores", force: :cascade do |t|
-    t.string "key", null: false
-    t.integer "value", default: 1, null: false
-    t.datetime "expires_at", null: false
     t.datetime "created_at", null: false
+    t.datetime "expires_at", null: false
+    t.string "key", null: false
     t.datetime "updated_at", null: false
+    t.integer "value", default: 1, null: false
     t.index ["expires_at"], name: "index_solid_queue_semaphores_on_expires_at"
     t.index ["key", "value"], name: "index_solid_queue_semaphores_on_key_and_value"
     t.index ["key"], name: "index_solid_queue_semaphores_on_key", unique: true
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email_address", null: false
-    t.string "password_digest", null: false
-    t.string "username", null: false
-    t.boolean "has_registered", null: false
-    t.string "student_id"
-    t.string "web_link"
-    t.boolean "is_staff", null: false
     t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string "description"
+    t.string "email_address", null: false
+    t.boolean "has_registered", null: false
+    t.boolean "is_staff", null: false
+    t.string "password_digest", null: false
+    t.string "student_id"
+    t.datetime "updated_at", null: false
+    t.string "username", null: false
+    t.string "web_link"
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
   end
 
