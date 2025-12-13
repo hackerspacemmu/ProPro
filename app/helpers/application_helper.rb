8ce9e4ec5c476d6c06f2dcdffc580bc8efe13c09
Ying Tong <yingtong0106@gmail.com>
2025-12-13 18:13:18 +0800

    Change active link in sidebar to darker blue

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,23 +6,21 @@ module ApplicationHelper
   end
 
   def sidebar_link(label, path)
-    base_classes = 'group flex items-center px-3 py-2 text-sm font-medium rounded-md transition-colors duration-150 ease-in-out w-full'
+    base_classes = 'group flex items-center px-3 py-4 text-sm font-medium rounded-md transition-colors duration-150 ease-in-out w-full'
 
-    # convert path to a string
+    # convert path to a string first to prevent parsing errors
     path_str = path.to_s
     is_active = if path_str.start_with?('#')
                   false
                 else
-                  # Returns true if you are on the page OR a sub-page.
-                  # e.g. Link '/projects' stays active when viewing '/projects/1'
                   request.path.start_with?(path_str) && path_str != '/'
                 end
 
     if is_active
-      active_classes = 'bg-blue-50 text-blue-700'
+      active_classes = 'bg-blue-200 text-blue-700'
       css_class = "#{base_classes} #{active_classes}"
     else
-      inactive_classes = 'text-gray-600 hover:bg-gray-50 hover:text-gray-900'
+      inactive_classes = 'text-gray-700 hover:bg-gray-50 hover:text-gray-900'
       css_class = "#{base_classes} #{inactive_classes}"
     end
 
