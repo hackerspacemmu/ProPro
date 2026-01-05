22defe4e6dafb3a0a2219a81193adad3206ca566
Ying Tong <yingtong0106@gmail.com>
2026-01-05 16:36:37 +0800

    Refactor breadcrumb to be more responsive on small screens

--- app/helpers/breadcrumb_helper.rb
+++ app/helpers/breadcrumb_helper.rb
@@ -3,24 +3,27 @@ module BreadcrumbHelper
     return unless breadcrumbs.present? && breadcrumbs.any?
 
     content_for(:breadcrumbs) do
-      elements = breadcrumbs.map.with_index do |crumb, index|
-        is_last = index == breadcrumbs.count - 1
+      tag.nav(class: 'flex flex-wrap items-center text-sm text-gray-500 w-full min-w-0 my-2') do
+        elements = breadcrumbs.map.with_index do |crumb, index|
+          is_last = index == breadcrumbs.count - 1
 
-        if is_last
-          tag.span(crumb.text, class: 'text-gray-900 font-medium truncate')
-        else
-          link = link_to(
-            crumb.text,
-            crumb.url,
-            class: 'text-gray-500 hover:text-gray-900 font-medium transition-colors no-underline'
-          )
-          separator = tag.span('>', class: 'px-3 text-gray-400')
+          if is_last
+            tag.span(crumb.text, class: 'text-gray-900 font-semibold break-all')
+          else
+            link = link_to(
+              crumb.text,
+              crumb.url,
+              class: 'hover:text-gray-900 font-medium transition-colors no-underline whitespace-nowrap'
+            )
 
-          safe_join([link, separator])
+            separator = tag.span('>', class: 'px-2 text-gray-500 text-xs select-none')
+
+            safe_join([link, separator])
+          end
         end
-      end
 
-      safe_join(elements)
+        safe_join(elements)
+      end
     end
   end
 end
