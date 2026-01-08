f22aa81057c25d82be2b17bd03c05a6ee4fb7b72
k3mystra <programmer.coleslaw981@slmail.me>
2026-01-09 03:10:52 +0800

    meta: Format and lint JS files

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,9 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
-import "project_template_fields"
+import "@hotwired/turbo-rails";
+import "controllers";
+import "project_template_fields";
+import htmx from "htmx.org";
 
-document.addEventListener('turbo:load', function() {
+document.addEventListener("turbo:load", function () {
   htmx.process(document.body);
 });
