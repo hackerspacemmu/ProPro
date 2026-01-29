f93248f1b7143fcebf001379dbce5c2a687ac522
JuneMinazuki <xtfrancis@gmail.com>
2026-01-29 22:35:09 +0800

    Adjusted spacing in the _copy_course_modal.html.erb

--- app/javascript/controllers/overlay_controller.js
+++ app/javascript/controllers/overlay_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["menu", "content"];
+  static targets = ["menu"];
 
   // Open the overlay
   open(e) {
