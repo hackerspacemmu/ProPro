8c755e1746923ba97cd0b8e42b7327599bad1de7
JuneMinazuki <xtfrancis@gmail.com>
2026-01-31 03:12:21 +0800

    Fix error invoking action "click->overlay#open"

--- app/javascript/controllers/overlay_controller.js
+++ app/javascript/controllers/overlay_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["menu"];
+  static targets = ["menu", "content"];
 
   // Open the overlay
   open(e) {
