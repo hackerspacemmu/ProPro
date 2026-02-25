439b26259bcea2bd9b4e49cc90762c6f9e8ef9ee
JuneMinazuki <xtfrancis@gmail.com>
2026-02-25 13:00:56 +0800

    Seperate both select event for overlay

--- app/javascript/controllers/overlay_controller.js
+++ app/javascript/controllers/overlay_controller.js
@@ -28,12 +28,15 @@ export default class extends Controller {
     }
   }
 
-  select(event) {
+  selectSetting(event) {
     const sourceCourseId = event.params.courseId;
     const targetCourseId = this.targetCourseIdValue
     const mode = this.modeValue;
     const frame = document.getElementById("overlay_content");
 
     frame.src = `/courses/${targetCourseId}/details?source_id=${sourceCourseId}&mode=${mode}`;
+  }
+
+  selectTopic(event) {
   }
 }
