25f5429972db8057ecdd89df160fc5914976a7cd
Alex <chiamjuinhoong@gmail.com>
2026-03-26 10:44:38 +0800

    fix: added error handling for user not authorized

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,9 @@ class ApplicationController < ActionController::Base
   include Pundit::Authorization
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+def user_not_authorized
+  redirect_to root_path, alert: "You are not authorized to view this page."
+end
 end
