7e599773385ea76fab1e541199288db6e1888b08
Ying Tong <yingtong0106@gmail.com>
2026-04-03 23:42:05 +0800

    fix: Add missing methods in error controller

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -2,4 +2,12 @@ class ErrorsController < ApplicationController
   def not_found; end
 
   def internal_server_error; end
+
+  def unprocessable_entity; end
+
+  def unauthorized; end
+
+  def forbidden; end
+
+  def service_unavailable; end
 end
