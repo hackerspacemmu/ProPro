620f7755ff803dec04ad2e49223699cf8f45ce49
Alex <chiamjuinhoong@gmail.com>
2026-04-19 10:14:14 +0800

    feat: added cannot_delete_title_field validation and added tests for all projecttemplate validations

--- test/factories/project_templates.rb
+++ test/factories/project_templates.rb
@@ -1,5 +1,16 @@
 FactoryBot.define do
-  factory :project_template do
-    association :course
+  factory :project_template_field do
+    association :project_template
+    label { 'Case Study Title' }
+    field_type { :shorttext }
+    applicable_to { :both }
+    required { false }
+    is_project_title { false }
+
+    trait :title_field do
+      label { 'Project Title' }
+      is_project_title { true }
+      required { true }
+    end
   end
 end
--- test/models/project_template_test.rb
+++ test/models/project_template_test.rb
@@ -1,7 +1,35 @@
 require 'test_helper'
 
-class ProjectTemplateTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+class ProjectTemplateFieldTest < ActiveSupport::TestCase
+  setup do
+    @course = create(:course)
+    @template = @course.project_template
+    @title_field = @template.project_template_fields.find_by(is_project_title: true)
+  end
+
+  test 'project template always has a title field after creation' do
+    assert_not_nil @title_field
+    assert @title_field.is_project_title?
+  end
+
+  test 'title field cannot be destroyed' do
+    assert_not @title_field.destroy
+    assert ProjectTemplateField.exists?(@title_field.id)
+  end
+
+  test 'title field remains required after being renamed' do
+    @title_field.update!(label: 'Study Name')
+    assert @title_field.reload.required?
+  end
+
+  test 'regular field can be destroyed if not in use' do
+    field = create(:project_template_field, project_template: @template)
+    assert field.destroy
+    assert_not ProjectTemplateField.exists?(field.id)
+  end
+
+  test 'is_project_title is false by default for new fields' do
+    field = create(:project_template_field, project_template: @template)
+    assert_not field.is_project_title?
+  end
 end
