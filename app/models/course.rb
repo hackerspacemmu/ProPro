5b99fe02d384d217fe3669014eda9a8c990b3134
Alex <chiamjuinhoong@gmail.com>
2026-08-21 15:58:22 +0800

    linter

--- app/models/course.rb
+++ app/models/course.rb
@@ -102,9 +102,9 @@ class Course < ApplicationRecord
 
   def unconfirmed_students_count
     confirmed_member_count = ProjectGroupMember
-                              .joins(:project_group)
-                              .where(project_groups: { course_id: id, confirmed: true })
-                              .count
+                             .joins(:project_group)
+                             .where(project_groups: { course_id: id, confirmed: true })
+                             .count
 
     students.count - confirmed_member_count
   end
--- app/models/project.rb
+++ app/models/project.rb
@@ -29,7 +29,7 @@ class Project < ApplicationRecord
 
   before_validation :set_ownership_type
 
-  validate :project_group_must_be_confirmed, if: -> { owner_type == "ProjectGroup" }
+  validate :project_group_must_be_confirmed, if: -> { owner_type == 'ProjectGroup' }
 
   STATUS_SORT_ORDER = { 'rejected' => 0, 'redo' => 1, 'pending' => 2, 'not_submitted' => 3, 'approved' => 4 }.freeze
 
@@ -84,8 +84,8 @@ class Project < ApplicationRecord
   def project_group_must_be_confirmed
     return if owner_id.blank?
 
-    if !group.confirmed?
-      errors.add(:owner_id, "cannot be a draft group")
-    end
+    return if group.confirmed?
+
+    errors.add(:owner_id, 'cannot be a draft group')
   end
 end
