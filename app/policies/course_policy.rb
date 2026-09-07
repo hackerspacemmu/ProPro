5e01c547394b36f34ca8104c54d8da399332a367
Alex <chiamjuinhoong@gmail.com>
2026-09-07 21:28:05 +0800

    linter

--- app/policies/course_policy.rb
+++ app/policies/course_policy.rb
@@ -71,6 +71,7 @@ class CoursePolicy < ApplicationPolicy
   def grouping?
     enrolment = record.enrolments.find_by(user:)
     return true if enrolment&.coordinator?
+
     enrolment.present? && record.grouping_enabled?
   end
 
